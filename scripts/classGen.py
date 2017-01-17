b6a7602d89646a282739dd29ba8ae4b276be092e
JohnfyBUPT <zhfy888@163.com>
2017-01-17 15:56:59 +0800

    preprocess

--- scripts/classGen.py
+++ scripts/classGen.py
@@ -3,83 +3,16 @@ import sys
 import re
 
 def main(argv):
-    img_dir = "madeups_gen/"
-    img_list = []
+    img_dir = "../data/chars_generated/"
     img_names = os.listdir(img_dir)
-    dic = {
-        '0': '0',
-        '1': '1',
-        '2': '2',
-        '3': '3',
-        '4': '4',
-        '5': '5',
-        '6': '6',
-        '7': '7',
-        '8': '8',
-        '9': '9',
-        'A': '10',
-        'B': '11',
-        'C': '12',
-        'D': '13',
-        'E': '14',
-        'F': '15',
-        'G': '16',
-        'H': '17',
-        'J': '18',
-        'K': '19',
-        'L': '20',
-        'M': '21',
-        'N': '22',
-        'P': '23',
-        'Q': '24',
-        'R': '25',
-        'S': '26',
-        'T': '27',
-        'U': '28',
-        'V': '29',
-        'W': '30',
-        'X': '31',
-        'Y': '32',
-        'Z': '33',
-        '藏': '34',
-        '川': '35',
-        '鄂': '36',
-        '甘': '37',
-        '赣': '38',
-        '贵': '39',
-        '桂': '40',
-        '黑': '41',
-        '沪': '42',
-        '吉': '43',
-        '冀': '44',
-        '津': '45',
-        '晋': '46',
-        '京': '47',
-        '辽': '48',
-        '鲁': '49',
-        '蒙': '50',
-        '闽': '51',
-        '宁': '52',
-        '青': '53',
-        '琼': '54',
-        '陕': '55',
-        '苏': '56',
-        '皖': '57',
-        '湘': '58',
-        '新': '59',
-        '渝': '60',
-        '豫': '61',
-        '粤': '62',
-        '云': '63',
-        '浙': '64'
-    }
-    file=open('classes.txt','w')
+    file=open('../data/classes.txt','w')
     is_jpg = re.compile(r'.+?\.jpg')
     print('generating class labels...') 
     if (len(img_names)>0):
         for name in img_names:
             if (is_jpg.match(name)):
-                file.write(dic[name[0]])
+                class_num = re.match(r'[0-9]*', name)
+                file.write(class_num.group(0))
                 file.write('\t')
     print('class label generated.')
 if __name__ == "__main__":
--- scripts/img2txt.py
+++ scripts/img2txt.py
@@ -3,12 +3,14 @@ import sys
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.misc 
+import math
 import re
 
 def main(argv):
-    img_dir = "madeups_gen/"
+    img_dir = "../data/chars_generated/"
     img_srcs = os.listdir(img_dir)
-    file=open('matrices.txt','w')
+    file=open('../data/matrices.txt','w')
     # leave files that are not jpg files
     is_jpg = re.compile(r'.+?\.jpg')
 
@@ -18,19 +20,74 @@ def main(argv):
             if(is_jpg.match(img_src)):
                 img_src = img_dir + img_src
                 img_mat = np.array(Image.open(img_src))
-                # if(img_mat.shape != (28, 18)):
-                #     print(img_src)
-                #     return
-                img_row = '\t'.join('\t'.join('%d' %x for x in y) for y in img_mat)
+                img_mat = img2directMap(img_mat)
+                img_row = '\t'.join('\t'.join('\t'.join('%d' %x for x in y) for y in z) for z in img_mat)
                 file.write(img_row)
                 file.write('\t')
-    file = open('matrices.txt')
-    img_row = file.readline().strip('\t').split('\t')
-    img_mat = np.asarray(img_row, dtype='int32')
-    print(img_mat.shape)
-    img_mat = np.reshape(img_mat, (-1, 28, 18))
     print('matrices generated.')
-    plt.imshow(img_mat[0])
-    plt.show()
+
+def img2directMap(img):
+    w_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+    w_x = np.array([[1, 0, -1],[2, 0, -2],[1, 0, -1]])
+    b = 0
+    img_g_abs, img_g_arc = sobel(img, w_x, w_y, b)
+    directMap = grad2directMap(img_g_abs, img_g_arc)
+    return directMap
+
+# sobel operation using conv            
+def sobel(x, w_x, w_y, b):
+    g_x = conv_forward_naive(x, w_x, b, {'stride': 1, 'pad': 1})
+    g_y = conv_forward_naive(x, w_y, b, {'stride': 1, 'pad': 1})
+    g_abs = np.abs(g_x) + np.abs(g_y) 
+    # use arctan2 to compute [-pi, pi] angles
+    g_arc = np.arctan2(g_y, g_x)
+    return (g_abs, g_arc)
+
+# map the gradients into directMap
+def grad2directMap(g_abs, g_arc):
+    pi = math.pi
+    directMap = np.zeros((8, g_abs.shape[0], g_abs.shape[1]))
+    arcs = np.array([0, pi/4, pi/2, pi*3/4, pi])
+    for i in range(4):
+        lbound = arcs[i]
+        ubound = arcs[i+1]
+        mask = (g_arc < ubound) * (g_arc >= lbound) 
+        directMap[i, :, :] += mask * g_abs * (np.cos(g_arc - lbound) - np.sin(g_arc - lbound))
+        directMap[i+1, :, :] += mask * g_abs * (np.cos(ubound - g_arc) - np.sin(ubound - g_arc))
+    arcs = np.array([-pi, -pi*3/4, -pi/2, -pi/4, 0])
+    for i in range(4):
+        lbound = arcs[i]
+        ubound = arcs[i+1]
+        mask = (g_arc < ubound) * (g_arc >= lbound) 
+        directMap[i+4, :, :] += mask * g_abs * (np.cos(g_arc - lbound) - np.sin(g_arc - lbound))
+        if(i == 3):
+            directMap[0, :, :] += mask * g_abs * (np.cos(ubound - g_arc) - np.sin(ubound - g_arc))
+        else:
+            directMap[i+5, :, :] += mask * g_abs * (np.cos(ubound - g_arc) - np.sin(ubound - g_arc))
+    directMap[directMap>255] = 255
+    return directMap
+
+# a naive conv forward    
+def conv_forward_naive(x, w, b, conv_param):
+    out = None
+    H, W = x.shape
+    HH, WW = w.shape
+    pad = conv_param['pad']
+    stride = conv_param['stride']
+
+    x_padded = np.pad(x, ((pad, pad), (pad, pad)), 'constant')
+    
+    Out_h = (int)(1 + (H + 2 * pad - HH) / stride)
+    Out_w = (int)(1 + (W + 2 * pad - WW) / stride)
+
+    out = np.zeros((Out_h, Out_w))
+
+    for k in range(Out_h):
+        for l in range(Out_w):
+            out[k, l] = np.sum(
+                x_padded[k * stride : k * stride + HH, l * stride : l * stride + WW] * w) + b
+            
+    return out
+
 if __name__ == '__main__':
     main(sys.argv[1:])
